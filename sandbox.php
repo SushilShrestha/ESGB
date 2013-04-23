5b26e17dc929fe6901a4a67828cdb057cff8855f
SushilShrestha <codegluttoners@gmail.com>
2013-04-23 22:46:29 +0545

    sandbox

---
+++ sandbox.php
@@ -0,0 +1,26 @@
+<?php
+
+try{
+	$db = new PDO("mysql:host=localhost;dbname=ESGB","root","");
+}catch(PDOException $e){
+	echo $e->getMessage();
+}
+
+try{
+	$db = new PDO('mysql:host=localhost;dbname=INFORMATION_SCHEMA',"root","");
+	
+	$sql = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'ESGB'";
+	$sql = "CREATE DATABASE IF NOT EXISTS ESGB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci";
+	$q = $db->query($sql) or die(print_r($db->errorInfo(), true));
+
+	echo "done";
+	if ($q->rowCount() == 0){
+		# create new database
+	}
+
+
+}catch(PDOException $e){
+	echo $e->getMessage();
+}
+
+?>
