8c346a120a49e1906a0b03fcdf7889f7c4d9f1f9
Sushil <sushil@sushil-P53G.(none)>
2013-04-29 20:18:06 +0545

    from desktop

--- sandbox.php
+++ sandbox.php
@@ -1,28 +1,14 @@
 <?php
 
-try{
-	$db = new PDO("mysql:host=localhost;dbname=ESGB","root","");
-}catch(PDOException $e){
-	echo $e->getMessage();
+function foo(){
+	echo func_num_args();
 }
-
-try{
-	$db = new PDO('mysql:host=localhost;dbname=INFORMATION_SCHEMA',"root","");
-	
-	$sql = "CREATE DATABASE IF NOT EXISTS ESGB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci";
-	$q = $db->query($sql) or die(print_r($db->errorInfo(), true));
-
-	echo "done";
-	if ($q->rowCount() == 0){
-		# create new database
-	}
+foo(2,4,5,'g');
 
 
-}catch(PDOException $e){
-	echo $e->getMessage();
-}
+
+// Call for volunteers,
+// We are builidng our very own Esgb website for that we are in need of some volunteers(esp. Designers). Intrested members please comment.
 
 ?>
 
-Call for volunteers,
-We are builidng our very own Esgb website for that we are in need of some volunteers(esp. Designers). Intrested members please comment.
